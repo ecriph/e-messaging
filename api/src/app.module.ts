3033ae1d5be8150e8c36ac9a7cdbeefb1c6418bc
Alozie Prince <ecriphlimited@gmail.com>
2023-09-25 14:11:37 +0100

    Project restructure

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, MessagesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
