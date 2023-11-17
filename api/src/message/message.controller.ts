afd944332945eb5565b93ab25d08e64ed63a2f6b
Alozie Prince <ecriphlimited@gmail.com>
2023-11-17 13:30:53 +0100

    created versioned routes and imported prisma to controller

--- api/src/message/message.controller.ts
+++ api/src/message/message.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Injectable } from '@nestjs/common';
+import { Controller, Get, Injectable } from '@nestjs/common';
+import { PrismaClientService } from 'src/internals/database/prisma-client.service';
 
 @Injectable()
-@Controller('message')
-export class MessageController {}
+@Controller({ path: 'message', version: '1' })
+export class MessageController {
+  constructor(private prisma: PrismaClientService) {}
+
+  @Get('/create')
+  async createMessage(): Promise<void> {}
+
+  @Get('/previous/messages')
+  async previousMessage(): Promise<void> {}
+
+  @Get('/conversation/send')
+  async sendMessage(): Promise<void> {}
+}
--- api/src/message/message.module.ts
+++ api/src/message/message.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessageController } from './message.controller';
+import { PrismaClientModule } from 'src/internals/database/prisma-client.module';
 
 @Module({
-  controllers: [MessageController]
+  imports: [PrismaClientModule],
+  controllers: [MessageController],
 })
 export class MessageModule {}
