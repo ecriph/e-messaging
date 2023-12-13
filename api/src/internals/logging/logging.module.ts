65c35018c3a3ffacd049ad832348517fec3fcfc6
Prince Alozie <74040127+ecriph@users.noreply.github.com>
2023-12-13 10:13:24 +0100

    Delete api directory

--- api/src/internals/logging/logging.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { LoggingService } from './logging.service';
-
-@Global()
-@Module({
-  providers: [LoggingService],
-  exports: [LoggingService],
-})
-export class LoggingModule {}
